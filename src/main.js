ad4099c28638b529277f268a26aff35361e8158f
Clément Catel <clement.catel4@gmail.com>
2020-03-14 20:11:23 +0100

    divers correctifs de style + mise à jour

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 Vue.config.productionTip = false;
+
+AOS.init({
+  delay: 100,
+  duration: 600
+});
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <v-app>
-    <v-app-bar app height="73" elevate-on-scroll >
-      <v-toolbar-title :class="{'px-10' : $vuetify.breakpoint.mdAndUp}" v-text="title" @click="goTo('#hero')"></v-toolbar-title>
+    <v-app-bar app height="73" elevate-on-scroll>
+      <v-toolbar-title
+        :class="{ 'px-10': $vuetify.breakpoint.mdAndUp }"
+        v-text="title"
+        @click="goTo('#hero')"
+      ></v-toolbar-title>
 
       <v-spacer></v-spacer>
 
@@ -11,9 +15,12 @@
         <v-btn text @click="goTo('#contact')">Contact</v-btn>
       </v-toolbar-items>
 
-      <v-menu >
+      <v-menu>
         <template v-slot:activator="{ on }">
-          <v-app-bar-nav-icon v-on="on" class="hidden-md-and-up"></v-app-bar-nav-icon>
+          <v-app-bar-nav-icon
+            v-on="on"
+            class="hidden-md-and-up"
+          ></v-app-bar-nav-icon>
         </template>
         <v-list>
           <v-list-item @click="goTo('#about-me')">
@@ -32,38 +39,55 @@
     <v-content>
       <section id="hero">
         <v-parallax src="../assets/images/hero.svg" height="675">
-            <v-layout
-              align-center
-              justify-center
-            >
-              <v-flex xs12 sm8>
-                <v-layout
-                  column
-                  align-left
-                  class="white--text"
+          <v-layout align-center justify-center>
+            <v-flex xs12 sm8>
+              <v-layout column align-left class="white--text">
+                <div class="text-left">
+                  <v-btn
+                    data-aos="fade-right"
+                    rounded
+                    dark
+                    small
+                    depressed
+                    class="mt-5 text-lowercase"
+                    color="rgba(255, 255, 255, 0.1)"
+                    href="mailto:clement.catel4@gmail.com"
+                    ><v-icon left small color="blue lighten-3">mdi-at</v-icon
+                    >clement.catel4@gmail.com</v-btn
+                  >
+                </div>
+                <h1
+                  class="display-3 my-5 mt-10 font-weight-light"
+                  data-aos="fade-right"
                 >
-                  <div class="text-left">
-                    <v-btn rounded dark small depressed class="mt-5 text-lowercase" color="rgba(255, 255, 255, 0.1)" href="mailto:clement.catel4@gmail.com"><v-icon left small color="blue lighten-3">mdi-at</v-icon>clement.catel4@gmail.com</v-btn>
-                  </div>
-                  <h1 class="display-3 my-5 mt-10 font-weight-light">Clément Catel</h1>
-                  <h2 class="my-5 headline text-left font-weight-light">Apprenti développeur Web</h2>
-                  
-                  <div class="text-left">
-                    <v-btn rounded dark large class="mt-5 elevation-5" color="blue lighten-2" @click="goTo('#about-me')">En savoir plus</v-btn>
-                  </div>
-                </v-layout>
-              </v-flex>
-            </v-layout>
+                  Clément Catel
+                </h1>
+                <h2
+                  class="my-5 headline text-left font-weight-light"
+                  data-aos="fade-right"
+                >
+                  Apprenti développeur Web
+                </h2>
+
+                <div class="text-left" data-aos="fade-right">
+                  <v-btn
+                    rounded
+                    dark
+                    large
+                    class="mt-5 elevation-5"
+                    color="blue lighten-2"
+                    @click="goTo('#about-me')"
+                    >En savoir plus</v-btn
+                  >
+                </div>
+              </v-layout>
+            </v-flex>
+          </v-layout>
         </v-parallax>
       </section>
 
       <section id="about-me">
-        <v-layout
-          wrap
-          class="my-12"
-          align-center
-          justify-center
-        >
+        <v-layout wrap class="my-12" align-center justify-center>
           <v-flex xs10 sm6 class="my-5 mb-12">
             <div class="text-center">
               <h3 class="display-3">A propos de moi</h3>
@@ -77,29 +101,63 @@
                     <v-layout justify-center>
                       <v-avatar
                         size="150"
-                        class="elevation-8"
+                        class="elevation-8 mb-5"
+                        data-aos="fade-down"
                       >
                         <img
-                        src="../assets/images/clementcatel.jpg"
-                        alt="clément catel"
-                        >
+                          src="../assets/images/clementcatel.jpg"
+                          alt="clément catel"
+                        />
                       </v-avatar>
                     </v-layout>
                     <!-- <v-card-title primary-title class="layout row justify-center mb-8">
                       <div class="display-1">Clément Catel</div>
                     </v-card-title> -->
                     <v-card-text class="body-1 mt-5">
-                      <p class="title font-weight-light">Hello! Moi c'est Clément Catel et je suis développeur Web junior. Passionné d'informatique, de nouvelles technologies et de design depuis maintenant plusieurs années, j'ai décidé d'orienter mon parcours 
-                      professionnel vers le monde de l'informatique et plus particulièrement celui du développement Web.</p>
-                      
-                      <p class="title font-weight-light">Je viens tout juste d'obtenir mon DUT Informatique. Formation durant laquelle j'ai pu acquérir une première expérience 
-                      en tant que développeur Web Frontend et Webdesigner aux côtés d'Alexandre Maurouard, au cours d'un stage de 3 mois dans sa société
-                      <a href="https://alexandremaurouard.fr/" target="_blank" class="link">Alexandre Maurouard Freelance</a>.</p>
+                      <p
+                        class="title font-weight-light py-3"
+                        data-aos="fade-right"
+                      >
+                        Hello! Je suis Clément Catel, développeur Passionné
+                        d'informatique, de nouvelles technologies et de design
+                        depuis plusieurs années, j'ai décidé d'orienter mon
+                        parcours professionnel vers le monde de l'informatique
+                        et plus particulièrement celui du développement Web.
+                      </p>
 
-                      <p class="title font-weight-light">
-                        Intégrant <a href="https://www.institut-g4.fr/" target="_blank" class="link">l'institut G4</a> de Paris pour la rentrée 2019, 
-                        et effectuant mon cursus en alternance, je recherche une entreprise dans laquelle je pourrai mettre à profit mes compétences et ma motivation 
-                        mais aussi enrichir mes connaissances ainsi que mon expérience. 
+                      <p
+                        class="title font-weight-light py-3"
+                        data-aos="fade-left"
+                      >
+                        Je suis titulaire d'un DUT Informatique. Formation
+                        durant laquelle j'ai pu acquérir une première expérience
+                        en tant que développeur Web Frontend et Webdesigner aux
+                        côtés d'Alexandre Maurouard, au cours d'un stage de 3
+                        mois dans sa société
+                        <a
+                          href="https://alexandremaurouard.fr/"
+                          target="_blank"
+                          class="link"
+                          >Alexandre Maurouard Freelance</a
+                        >.
+                      </p>
+
+                      <p
+                        class="title font-weight-light py-3"
+                        data-aos="fade-right"
+                      >
+                        Je prépare actuellement un diplôme de chef de projet SI
+                        au sein d'une formation de 3 ans, à
+                        <a
+                          href="https://www.institut-g4.fr/"
+                          target="_blank"
+                          class="link"
+                          >l'institut G4</a
+                        >
+                        de Paris. Effectuant mon cursus en alternance, je
+                        recherche une entreprise dans laquelle je pourrai mettre
+                        à profit mes compétences et ma motivation mais aussi
+                        enrichir mes connaissances ainsi que mon expérience.
                       </p>
                     </v-card-text>
                   </v-card>
@@ -119,9 +177,10 @@
           </v-flex>
           <v-flex xs12 sm10 md8 xl6>
             <v-container grid-list-sm fluid class="px-0">
-              <v-layout wrap justify-center class="mx-0">
+              <v-layout wrap justify-center class="mx-0" data-aos="fade-up">
                 <v-flex
-                  v-for="(project, index) in projects" :key="index"
+                  v-for="(project, index) in projects"
+                  :key="index"
                   xs10
                   sm8
                   md6
@@ -130,32 +189,35 @@
                   child-flex
                 >
                   <v-hover v-slot:default="{ hover }">
-                    <v-card
-                      tile
-                      :elevation="hover ? 12 : 2"
-                    >
+                    <v-card tile :elevation="hover ? 12 : 2">
                       <v-img
                         :src="project.image"
                         aspect-ratio="1"
                         class="grey lighten-2"
                       >
                         <v-fade-transition>
-                          <v-overlay 
+                          <v-overlay
                             v-if="hover"
                             absolute
                             color="primary"
                             opacity="0.95"
                           >
-                            <v-card-title class="title white--text fill-height absolute">
+                            <v-card-title
+                              class="title white--text fill-height absolute"
+                            >
                               <v-layout
                                 fill-height
                                 column
                                 justify-space-between
                               >
-                                <p class="mt-4 subheading text-left">{{ project.title }}</p>
+                                <p class="mt-4 subheading text-left">
+                                  {{ project.title }}
+                                </p>
 
                                 <div>
-                                  <p class="body-1 font-weight-light text-left mb-0">
+                                  <p
+                                    class="body-1 font-weight-light text-left mb-0"
+                                  >
                                     {{ project.description }}
                                   </p>
                                 </div>
@@ -168,7 +230,9 @@
                                     :href="project.sourcelink"
                                     target="_blank"
                                   >
-                                    <v-icon class="white--text">mdi-code-tags</v-icon>
+                                    <v-icon class="white--text"
+                                      >mdi-code-tags</v-icon
+                                    >
                                   </v-btn>
                                   <v-btn
                                     large
@@ -190,7 +254,10 @@
                             justify-center
                             ma-0
                           >
-                            <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                            <v-progress-circular
+                              indeterminate
+                              color="grey lighten-5"
+                            ></v-progress-circular>
                           </v-layout>
                         </template>
                       </v-img>
@@ -204,12 +271,7 @@
       </section>
 
       <section id="contact">
-        <v-layout
-          row
-          class="my-12 mx-0"
-          align-center
-          justify-center
-        >
+        <v-layout row class="my-12 mx-0" align-center justify-center>
           <v-flex xs10 sm6 class="my-5 mb-12">
             <div class="text-center">
               <h3 class="display-3">Me contacter</h3>
@@ -218,7 +280,12 @@
           <v-flex xs12>
             <v-layout row justify-center class="mx-0">
               <v-flex xs10 md6>
-                <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+                <form
+                  name="contact"
+                  method="POST"
+                  data-netlify="true"
+                  data-netlify-honeypot="bot-field"
+                >
                   <input type="hidden" name="form-name" value="contact" />
                   <v-text-field
                     label="Nom"
@@ -232,13 +299,17 @@
                     name="email"
                     solo
                   ></v-text-field>
-                  <v-textarea
-                    solo
-                    name="message"
-                    label="Message"
-                  ></v-textarea>
+                  <v-textarea solo name="message" label="Message"></v-textarea>
                   <v-layout justify-center>
-                    <v-btn dark rounded large class="mt-5 elevation-5" color="blue lighten-2" type="submit">Envoyer</v-btn>
+                    <v-btn
+                      dark
+                      rounded
+                      large
+                      class="mt-5 elevation-5"
+                      color="blue lighten-2"
+                      type="submit"
+                      >Envoyer</v-btn
+                    >
                   </v-layout>
                 </form>
               </v-flex>
@@ -247,33 +318,25 @@
         </v-layout>
       </section>
 
-      <v-footer dark padless class="primary white--text text-center">
-        <v-layout
-          justify-center
-          wrap
-        >
+      <v-footer dark padless class="primary white--text text-center pt-3">
+        <v-layout justify-center wrap>
           <v-btn
-              class="mx-4 white--text"
-              icon
-              href="https://www.linkedin.com/in/cl%C3%A9ment-catel/"
-              target="_blank"
-            >
-              <v-icon size="24px">mdi-linkedin</v-icon>
-            </v-btn>
-            <v-btn
-              class="mx-4 white--text"
-              icon
-              href="https://github.com/ClementCatel"
-              target="_blank"
-            >
-              <v-icon size="24px">mdi-github-circle</v-icon>
-            </v-btn>
-          <v-flex
-            py-4
-            text-center
-            white--text
-            xs12
+            class="mx-4 white--text"
+            icon
+            href="https://www.linkedin.com/in/cl%C3%A9ment-catel/"
+            target="_blank"
           >
+            <v-icon size="24px">mdi-linkedin</v-icon>
+          </v-btn>
+          <v-btn
+            class="mx-4 white--text"
+            icon
+            href="https://github.com/ClementCatel"
+            target="_blank"
+          >
+            <v-icon size="24px">mdi-github-circle</v-icon>
+          </v-btn>
+          <v-flex py-4 text-center white--text xs12>
             {{ new Date().getFullYear() }} © Clément Catel
           </v-flex>
         </v-layout>
@@ -289,43 +352,46 @@ export default {
       title: "Clément Catel",
       projects: [
         {
-          image: require('../assets/images/personnal-site.png'),
+          image: require("../assets/images/personnal-site.png"),
           title: "Site Portfolio",
-          description: "Site portfolio personnel utilisant le framework Vue.js avec Vuetify",
+          description:
+            "Site portfolio personnel utilisant le framework Vue.js avec Vuetify",
           sourcelink: "https://github.com/ClementCatel/personnal-site",
-          link: "#",
+          link: "#"
         },
         {
-          image: require('../assets/images/resto-fontaines.png'),
+          image: require("../assets/images/resto-fontaines.png"),
           title: "Restaurant Le F Des Fontaines",
-          description: "Réalisé lors de mon stage chez Alexandre Maurouard Freelance, en collaboration avec Alexandre Maurouard et un autre stagiaire",
+          description:
+            "Réalisé lors de mon stage chez Alexandre Maurouard Freelance, en collaboration avec Alexandre Maurouard et un autre stagiaire",
           sourcelink: null,
-          link: "https://restaurant-les-fontaines.com/",
+          link: "https://restaurant-les-fontaines.com/"
         },
         {
-          image: require('../assets/images/nsc.png'),
+          image: require("../assets/images/nsc.png"),
           title: "Noël Sciage Carrotage",
-          description: "Réalisé lors de mon stage chez Alexandre Maurouard Freelance, en collaboration avec Alexandre Maurouard et un autre stagiaire",
+          description:
+            "Réalisé lors de mon stage chez Alexandre Maurouard Freelance, en collaboration avec Alexandre Maurouard et un autre stagiaire",
           sourcelink: null,
-          link: "https://noelsciagecarottage.fr/",
-        },
+          link: "https://noelsciagecarottage.fr/"
+        }
       ]
-    }
+    };
   },
-  
+
   methods: {
     goTo(dest) {
-      this.$vuetify.goTo(dest, { duration: 800, easing: 'easeInOutCubic'});
+      this.$vuetify.goTo(dest, { duration: 800, easing: "easeInOutCubic" });
     }
   }
 };
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap');
+@import url("https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap");
 
 .v-toolbar__title {
-  font-family: 'Josefin Sans', sans-serif;
+  font-family: "Josefin Sans", sans-serif;
   font-weight: 600;
   font-size: 26px;
   cursor: pointer;
@@ -335,7 +401,8 @@ export default {
   color: inherit;
   text-decoration: none;
   font-weight: 600;
-} .link:hover {
+}
+.link:hover {
   color: black;
 }
 
